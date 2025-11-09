0400aea942ef349508afb4aaf3d7473ffd20f605
Zady <zlobodys@gmail.com>
2025-11-09 15:56:58 -0600

    Added the Items of the program to be used

---
+++ CPrograms/GroceryShopping.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*
+Items to buy
+How much the items cost
+
+The user budget: Starting point $100
+Users reciept
+
+
+*/
+
+main () {
+
+    float UserBudget = 100.0;
+    float UserReciept = 0.0;
+
+    float Items[] = {.99, 8.99, 2.99, 1.99};
+    char Apple[5] = "Apple";
+    char Chicken[7] = "Chicken";
+    char Bread[5] = "Bread";
+    char Carrot[6] = "Carrot";
+
+
+
+
+    return 0;
+}
