62da7c230bd40ea71902b754978239007018ea3c
Zady <zlobodys@gmail.com>
2025-11-11 22:27:15 -0600

    Finished

--- CPrograms/GroceryShopping.c
+++ CPrograms/GroceryShopping.c
@@ -27,14 +27,14 @@ int main () {
 
     printf("We are very limited store... so sorry in advance:)\n");
     printf("Your options from this store are: %s, %s, %s, %s, %s\n", Apple, Chicken, Bread, Carrot, Milk);
-    printf("The cost of each item \n{\n %s: %c%.2f,\n %s: %c%.2f,\n %s: %c%.2f,\n %s: %c%.2f,\n %s: %c%.2f\n}", Apple, money, Items[0], Chicken, money, Items[1],
+    printf("The cost of each item \n{\n %s: %c%.2f,\n %s: %c%.2f,\n %s: %c%.2f,\n %s: %c%.2f,\n %s: %c%.2f\n}\n", Apple, money, Items[0], Chicken, money, Items[1],
     Bread, money, Items[2], Carrot, money, Items[3], Milk, money, Items[4]);
     printf("Whats your name?\n");
     scanf("%s", UserName);
 
 
     printf("To shop at this store you must buy all in ascending order. Would you like to still shop? Y/N\n");
-    scanf("%s\n", UserInput);
+    scanf("%s", UserInput);
     printf("Muhhaha I don't care! You must buy all and play the game! Unless you ctrl C which you wouldn't do that... Right? Y/N\n");
     scanf("%s", UserInput);
 
@@ -44,6 +44,19 @@ int main () {
     printf("Did you say no? Oopsie;) If you said yes well congrats you have bread now. Feed your family!\n");
     printf("How about anyyy fruit!! You know an apple a day keeps the doctor away!! Would you like an apple? Y/N\n");
     scanf("%s", UserInput);
+    UserBudget = UserBudget - Items[0];
+
+    printf("OOOOOOWEEEE taking your money and giving you your item is a great feeling!! We doing great business for one another!!\n");
+    printf("Kepp giving me your moooneyyy! You've only bought 2 Items and this is what you account looks like: %f", UserBudget);
+    printf("Okay now that you know where you are at it's not even that bad right!! Now time to buy some Protien for those muscles! Y/N\n");
+    scanf("%s", UserInput);
+    UserBudget = UserBudget - Items[1];
+
+    printf("I'm getting tired of this game im just gonna take the rest of your money! Muwhahhahaha!!\n");
+    printf("I'll show you what you are at now then I'll show you what I'm gonna take!!: %f\n", UserBudget);
+    UserBudget = UserBudget - UserBudget;
+    printf("Your account now! %f\n", UserBudget);
+    printf("Thanks for playing %s\n", UserName);
 
     return 0;
 }
